9eb1fafb7ef45cf11b5af3fe603cf5efa3951780
pulluripranavi <pulluripranavi@gmail.com>
2026-01-24 13:25:20 +0530

    Add 69-Sqrtx.py

---
+++ 69-Sqrtx/69-Sqrtx.py
@@ -0,0 +1,8 @@
+# Last updated: 6/2/2026, 12:04:02 PM
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        return int(x**0.5)
