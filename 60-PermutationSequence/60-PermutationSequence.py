39fa72791ab6cf1fa46e937ff5fc7c85376557c7
pulluripranavi <pulluripranavi@gmail.com>
2026-01-29 09:17:01 +0530

    Add 60-PermutationSequence.py

---
+++ 60-PermutationSequence/60-PermutationSequence.py
@@ -0,0 +1,16 @@
+# Last updated: 6/2/2026, 12:04:16 PM
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        nums = [str(i) for i in range(1, n + 1)]
+        fact = [1] * (n + 1)
+        for i in range(1, n + 1):
+            fact[i] = fact[i - 1] * i
+        
+        k -= 1  
+        ans = []
+        for i in range(n, 0, -1):
+            idx = k // fact[i - 1]
+            ans.append(nums[idx])
+            nums.pop(idx)
+            k %= fact[i - 1]
+        return ''.join(ans)
