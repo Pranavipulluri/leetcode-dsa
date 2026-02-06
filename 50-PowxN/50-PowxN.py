54d7f7500e5678813cf201592eaadc270b47c9bb
pulluripranavi <pulluripranavi@gmail.com>
2026-02-06 16:53:59 +0530

    Add 50-PowxN.py

---
+++ 50-PowxN/50-PowxN.py
@@ -0,0 +1,14 @@
+# Last updated: 6/2/2026, 12:04:30 PM
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n < 0:
+            x = 1 / x
+            n = -n
+        result = 1
+        current_product = x
+        while n > 0:
+            if n % 2 == 1:
+                result = result * current_product
+            current_product = current_product * current_product
+            n = n // 2
+        return result
