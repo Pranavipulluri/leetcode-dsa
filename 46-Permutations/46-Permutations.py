e8fd007da5f04f5cd18af29efe1bac4387f76bd6
pulluripranavi <pulluripranavi@gmail.com>
2026-02-07 17:33:20 +0530

    Add 46-Permutations.py

---
+++ 46-Permutations/46-Permutations.py
@@ -0,0 +1,11 @@
+# Last updated: 6/2/2026, 12:04:36 PM
+from itertools import permutations
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return list(permutations(nums))
+
+        
