4ccc38e3b9367da91389880b48fc1e1679344894
pulluripranavi <pulluripranavi@gmail.com>
2026-02-12 09:20:12 +0530

    Add 43-MultiplyStrings.py

---
+++ 43-MultiplyStrings/43-MultiplyStrings.py
@@ -0,0 +1,12 @@
+# Last updated: 6/2/2026, 12:04:40 PM
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        n1=int(num1)
+        n2=int(num2)
+        return str(n1*n2)
+        
