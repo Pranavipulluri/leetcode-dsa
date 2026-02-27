74f01d43300d131ac53ccde591a0b7f374885022
pulluripranavi <pulluripranavi@gmail.com>
2026-02-27 18:46:28 +0530

    Add 4175-SmallestAllOnesMultiple.py

---
+++ 4175-SmallestAllOnesMultiple/4175-SmallestAllOnesMultiple.py
@@ -0,0 +1,15 @@
+# Last updated: 6/2/2026, 12:00:57 PM
+class Solution(object):
+    def minAllOneMultiple(self, k):
+        """
+        :type k: int
+        :rtype: int
+        """
+        if k%2==0 or k%5==0:
+            return -1
+        rem=0
+        for l in range(1,k+1):
+            rem=(rem*10+1)%k
+            if rem==0:
+                return l
+        return -1
