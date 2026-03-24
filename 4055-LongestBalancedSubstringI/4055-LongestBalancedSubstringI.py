552ac517f2bffd1510170551789a1dcfd9e74c1a
pulluripranavi <pulluripranavi@gmail.com>
2026-03-24 17:47:26 +0530

    Add 4055-LongestBalancedSubstringI.py

---
+++ 4055-LongestBalancedSubstringI/4055-LongestBalancedSubstringI.py
@@ -0,0 +1,12 @@
+# Last updated: 6/2/2026, 12:01:44 PM
+class Solution:
+    def longestBalanced(self, s: str) -> int:
+        n = len(s)
+        res = 0
+        for i in range(n):
+            cnt = defaultdict(int)
+            for j in range(i, n):
+                cnt[s[j]] += 1
+                if len(set(cnt.values())) == 1:
+                    res = max(res, j - i + 1)
+        return res
