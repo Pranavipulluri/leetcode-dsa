f23fc94255406e142befe11db1790289b6eb44f7
pulluripranavi <pulluripranavi@gmail.com>
2026-03-29 12:29:28 +0530

    Add 4024-MinimumCostToSplitIntoOnes.py

---
+++ 4024-MinimumCostToSplitIntoOnes/4024-MinimumCostToSplitIntoOnes.py
@@ -0,0 +1,8 @@
+# Last updated: 6/2/2026, 12:01:49 PM
+class Solution(object):
+    def minCost(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return n*(n-1)//2
