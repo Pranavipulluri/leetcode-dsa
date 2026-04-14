c88c9b8c8521365cae0e5f44fbdc913042dcdb96
pulluripranavi <pulluripranavi@gmail.com>
2026-04-14 14:29:16 +0530

    Add 31-NextPermutation.py

---
+++ 31-NextPermutation/31-NextPermutation.py
@@ -0,0 +1,18 @@
+# Last updated: 6/2/2026, 12:04:57 PM
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        i = len(nums) - 1
+        while i > 0 and nums[i-1] >= nums[i]:
+            i -= 1
+        
+        if i == 0:
+            nums.reverse()
+            return
+        
+        j = len(nums) - 1
+        while j >= i and nums[j] <= nums[i-1]:
+            j -= 1
+        
+        nums[i-1], nums[j] = nums[j], nums[i-1]
+
+        nums[i:] = reversed(nums[i:])
