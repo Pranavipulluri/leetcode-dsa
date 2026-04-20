16015b6c4005399250aefb4115da9346468011b7
pulluripranavi <pulluripranavi@gmail.com>
2026-04-20 11:54:40 +0530

    Add 24-SwapNodesInPairs.py

---
+++ 24-SwapNodesInPairs/24-SwapNodesInPairs.py
@@ -0,0 +1,23 @@
+# Last updated: 6/2/2026, 12:05:08 PM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        prev, cur = dummy, head
+
+        while cur and cur.next:
+            npn = cur.next.next
+            second = cur.next
+
+            second.next = cur
+            cur.next = npn
+            prev.next = second
+
+            prev = cur
+            cur = npn
+        
+        return dummy.next
