5de763df9ad65ccd1b92767ba81e15f0e50e6964
pulluripranavi <pulluripranavi@gmail.com>
2026-05-05 15:36:10 +0530

    Add 1500-CountLargestGroup.py

---
+++ 1500-CountLargestGroup/1500-CountLargestGroup.py
@@ -0,0 +1,13 @@
+# Last updated: 6/2/2026, 12:02:26 PM
+from collections import defaultdict
+
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        digit_sum_groups = defaultdict(int)
+
+        for i in range(1, n + 1):
+            digit_sum = sum(int(d) for d in str(i))
+            digit_sum_groups[digit_sum] += 1
+
+        max_size = max(digit_sum_groups.values())
+        return sum(1 for size in digit_sum_groups.values() if size == max_size)
